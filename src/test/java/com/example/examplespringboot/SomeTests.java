7c4c4e249bd4e0fb816fee2e70fe9d8658e9c9c3
Holger Jannasch <holger.jannasch@bs.ch>
2024-01-03 14:58:13 +0100

    Add surefire

---
+++ src/test/java/com/example/examplespringboot/SomeTests.java
@@ -0,0 +1,14 @@
+package com.example.examplespringboot;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeTests {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
